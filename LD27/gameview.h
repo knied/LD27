0d8dc46419048964f6f5e46584f7348100d73834
Kristof Niederholtmeyer <kristof.niederholtmeyer@rwth-aachen.de>
2013-08-16 23:50:10 +0200

    Basic Qt and OpenGL setup for LD27

---
+++ LD27/gameview.h
@@ -0,0 +1,53 @@
+// =================================================================
+// Author: Kristof Niederholtmeyer
+//
+
+#ifndef GAMEVIEW_H
+#define GAMEVIEW_H
+
+// Make Qt load the OpenGL 3 functions
+#define GL3_PROTOTYPES
+
+#include <QGLWidget>
+#include <vector>
+#include <sys/time.h>
+
+class GameView : public QGLWidget
+{
+    Q_OBJECT
+public:
+    explicit GameView(const QGLFormat& format, QWidget *parent = 0);
+
+    int prefered_width() const;
+    int prefered_height() const;
+protected:
+    void initializeGL();
+    void resizeGL(int width, int height);
+    void paintGL();
+
+    void showEvent(QShowEvent *event);
+    void timerEvent(QTimerEvent *event);
+    void keyPressEvent(QKeyEvent *event);
+    void keyReleaseEvent(QKeyEvent *event);
+private:
+    void compile_shader(GLuint shader_identifier, const std::string& shader_source);
+    void link_program();
+    void update_vertex_buffer();
+
+    int _tile_size;
+
+    timeval _lastTime;
+
+    int _timer_id;
+    GLuint _vao;
+    GLuint _vertex_buffer_identifier;
+    GLuint _program_identifier;
+    GLuint _texture_identifier;
+    
+signals:
+    
+public slots:
+    
+};
+
+#endif // GAMEVIEW_H
---
+++ LD27/main.cpp
@@ -0,0 +1,17 @@
+// =================================================================
+// Author: Kristof Niederholtmeyer
+//
+
+#include "mainwindow.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    Q_INIT_RESOURCE(resources);
+    
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    
+    return a.exec();
+}
